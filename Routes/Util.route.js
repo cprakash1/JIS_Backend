5361e04dd2b495a0cd7944677933a427e578d835
Chandra\nPrakash <chandraprakash47637@gmail.com>
2024-03-24 20:58:50 +0530

    Phase 3 error removed

--- Routes/Util.route.js
+++ Routes/Util.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router({ mergeParams: false });
 
-const UtilController = require("../Controllers/Util.controller");
+const UtilController = require("../Controllers/Util.Controller");
 
 router.get("/court", UtilController.getAllCourts);
 router.post("/judge-lawyer", UtilController.getCourtJudgesLawyers);
